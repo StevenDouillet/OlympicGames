195f06aeba06d46d862fba23846c729037108386
DOUILLET <Steven.douillet@gmail.com>
2022-02-08 15:08:19 +0100

    rework webhook endpoint

--- routes/webhook/sports.js
+++ routes/webhook/sports.js
@@ -2,17 +2,50 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async function (req, res, next) {
-    res.json({
-        fulfillmentMessages: [
-            {
-                text: {
-                    text: [
-                        'Im here, test'
-                    ]
-                }
-            }
-        ]
-    });
+    const action = request.body.queryResult.action || 'unknown';
+
+    switch (action) {
+        case 'sports':
+            response.json({
+                fulfillmentMessages: [
+                    {
+                        text: {
+                            text: [
+                                'Tu as choisi de voir les sports 🏀'
+                            ]
+                        }
+                    }
+                ]
+            });
+            break;
+
+        case 'athletes':
+            response.json({
+                fulfillmentMessages: [
+                    {
+                        text: {
+                            text: [
+                                'Tu as choisi de voir les athlètes 🏋🏻‍♀️'
+                            ]
+                        }
+                    }
+                ]
+            });
+            break;
+
+        default:
+            response.json({
+                fulfillmentMessages: [
+                    {
+                        text: {
+                            text: [
+                                'Je ne suis pas sûr d\'avoir la réponse...'
+                            ]
+                        }
+                    }
+                ]
+            });
+    }
 });
 
 module.exports = router;
